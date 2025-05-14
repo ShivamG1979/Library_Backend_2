174bc3d787571d994b7ae7814ad4524fd514228b
ShivamG1979 <shivamg1504@gmail.com>
2025-05-14 13:06:06 +0530

    updtae

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ connectDB();
 
 const app = express();
 app.use(express.json()); 
-app.use(cors()); 
+app.use(cors({
+  origin: 'http://localhost:5173', 
+}));
+
 
 // Routes
 app.use('/api/auth', authRoutes);
