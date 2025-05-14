7247ccdf94f976618a0e8ae9baa0d93ae05d20e8
ShivamG1979 <shivamg1504@gmail.com>
2025-05-14 13:19:23 +0530

    updtae

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ connectDB();
 const app = express();
 app.use(express.json()); 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173', 'https://library-frontend-2.vercel.app'],
 }));
+
 
 
 // Routes
